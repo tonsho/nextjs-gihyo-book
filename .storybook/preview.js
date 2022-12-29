52e58bab08f6ce7f6f15762c03cba9df28b1207d
Kazutaka Tonsho <kazutaka.tonsho@sony.com>
2022-12-29 19:29:56 +0900

    add theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from "@storybook/react"
+import * as NextImage from "next/image"
+import { ThemeProvider, createGlobalStyle } from "styled-components"
+import { theme } from "../src/themes"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,43 @@ export const parameters = {
     },
   },
 }
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`
+
+// Theme の適用
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+))
+
+// next/image の差し替え
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) =>
+    typeof props.src === "string" ? (
+      <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+    ) : (
+      <OriginalNextImage {...props} unoptimized />
+    ),
+})
