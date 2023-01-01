362b609a67de118fd9a90fa837736b0d37736fb6
Kazutaka Tonsho <kazutaka.tonsho@sony.com>
2023-01-01 18:55:32 +0900

    Button

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,68 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Button from '.'
+
+export default {
+  title: 'Atom/Button',
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' },
+      defaultValue: 'primary',
+      description: 'ボタンバリアント',
+      table: {
+        type: { summary: 'primary | secondary' },
+        defaultValue: { summary: 'primary' },
+      },
+    },
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'Button',
+      description: 'ボタンテキスト',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description: 'Disabledフラグ',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
+    width: {
+      control: { type: 'number' },
+      description: '横幅',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      control: { type: 'number' },
+      description: '縦幅',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    onClick: {
+      description: 'onClickイベントハンドラ',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+// プライマリボタン
+export const Primary = Template.bind({})
+Primary.args = { variant: 'primary', children: 'Primary Button' }
+
+// セカンダリボタン
+export const Secondary = Template.bind({})
+Secondary.args = { variant: 'secondary', children: 'Secondary Button' }
+
+// 無効化ボタン
+export const Disabled = Template.bind({})
+Disabled.args = { disabled: true, children: 'Disabled Button' }
